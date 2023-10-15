686878f55d6d12c5823aa912e7f0c0af7e1892e5
devEliasJr <eliasengmecanica@gmail.com>
2023-10-15 00:13:02 -0300

    feat: ex002 horas e time zones

--- ex001/index.php
+++ ex001/index.php
@@ -1,13 +1,16 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Dados do Servidor</title>
 </head>
+
 <body>
-  <?php 
-    phpinfo();
+  <?php
+  phpinfo();
   ?>
 </body>
+
 </html>
