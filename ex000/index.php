ef61c0d06d9bf5b72f14f6b285c4e3126a473a76
devEliasJr <eliasengmecanica@gmail.com>
2023-10-14 23:56:55 -0300

    feat: Ex000

---
+++ ex000/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>PHP - Aula 1</title>
+</head>
+<body>
+  <h1>
+    <?php 
+      echo "Hello World \u{1F30E}";
+    ?>
+  </h1>
+  <p>Paragrafro</p>
+</body>
+</html>
