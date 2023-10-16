fe811cce1396fc5dfde3f2b34b710afd0cb4a365
devEliasJr <eliasengmecanica@gmail.com>
2023-10-15 21:00:35 -0300

    feat: ex003 tipos primitivos

---
+++ ex003/index.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Tipos Primitivos</title>
+</head>
+
+<body>
+  <h1>Testes de tipos primitivos</h1>
+  <?php
+  // $num = 10;
+  // echo "O valor da variavel é $num";
+
+  // $v = 300;
+  // var_dump($v);
+
+  #coersão
+  //   $num = (int) 3e2;
+  //   var_dump($num);
+
+  //   $numCoersao = (int) "900";
+  //   var_dump($numCoersao);
+
+  // $casado = true;
+  // echo "O valor para casado é $casado";
+
+  // $vet = [6, 2.3, "Elias", 1, 9, true];
+  // echo $vet;
+  // var_dump($vet);
+
+  // class Pessoa {
+  //   private string $nome;
+  // }
+
+  // $p = new Pessoa();
+  // var_dump($p)
+
+  #String Heredoc
+
+  // $name = "Elias";
+  // $lastname = "Junior";
+
+  // echo <<<FULLNAME
+  //     O meu nome é:
+  //       $name
+  //       $lastname
+  //       \u{1F956}
+  // FULLNAME;
+
+  ?>
+</body>
+
+</html>
