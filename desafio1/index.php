4d505697196778072aea2d14d95bb6178516ccdd
devEliasJr <eliasengmecanica@gmail.com>
2023-10-16 23:15:27 -0300

    feat: desafio1 antecessor e sucessor

---
+++ desafio1/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Antecessor e sucessor</title>
+  <link rel="stylesheet" href="../styles/style.css" />
+</head>
+
+<body>
+  <header>
+    <h1>Antecessor e sucessor</h1>
+  </header>
+  <section>
+    <form action="verificar.php" method="get">
+      <label for="number">Numero</label>
+      <input type="number" name="number" id="number">
+      <input type="submit" value="Enviar">
+    </form>
+  </section>
+</body>
+</html>
